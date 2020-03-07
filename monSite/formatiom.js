6044998bd1ff8fe3114acd7c91a6eb4febf329b8
mohamedSa2000 <mido.sehli.msgf@gmail.com>
2020-03-07 17:47:20 +0100

    first commit

---
+++ monSite/formatiom.js
@@ -0,0 +1,13 @@
+import React from 'react' ; 
+import formation from  ''
+Class formation extends React.Component{ 
+	render()
+		return(
+			<div>
+				<h1>class based Component</h1>
+				<cla
+			</div>
+			)
+		)
+}
+export default App  ;
---
+++ monSite/main.js
@@ -0,0 +1,4 @@
+import React from 'React';
+function App(){
+	
+}
